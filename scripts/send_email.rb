e2ee6f1beba13d578832ce0dd29ecbfce328d1b1
DC <dc-bk@example.com>
2025-12-19 12:34:25 -0300

    Allow contact by email

---
+++ scripts/send_email.rb
@@ -0,0 +1,27 @@
+require 'net/smtp'
+
+smtp_host = ENV.fetch("SMTP_HOST")
+smtp_port = ENV.fetch("SMTP_PORT")
+
+reply_to_email = ENV.fetch("REPLY_TO_EMAIL")
+reply_to_name  = ENV.fetch("REPLY_TO_NAME")
+from_email     = ENV.fetch("FROM_EMAIL")
+to_email       = ENV.fetch("TO_EMAIL")
+subject        = ENV.fetch("SUBJECT")
+body           = ENV.fetch("BODY")
+
+smtp_user   = ENV.fetch("SMTP_USER")
+smtp_secret = ENV.fetch("SMTP_SECRET")
+
+message = <<~MSG
+  From: #{from_email}
+  Reply-To: "#{reply_to_name}" <#{reply_to_email}>
+  To: #{to_email}
+  Subject: #{subject}
+
+  #{body}
+MSG
+
+Net::SMTP.start(smtp_host, smtp_port, user: smtp_user, secret: smtp_secret) do |smtp|
+  smtp.send_message(message, from_email, to_email)
+end
